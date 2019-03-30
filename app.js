6a0b94da915ed3a8cde4cc04fbe3084aa9b80bd5
Marcus Galvin <marcusgalvin.student@careerdevs.com>
2019-03-30 17:15:39 +0000

    lesson 6 code

--- app.js
+++ app.js
@@ -1,21 +1,8 @@
-const bookList = document.querySelector('#book-list');
+const banner = document.querySelector('#page-banner');
 
-console.log('book list parent element:', bookList.parentElement);
-console.log('book list parent node:', bookList.parentNode);
+console.log('#page-banner node type is:', banner.nodeType);
+console.log('#page-banner node name is:', banner.nodeName);
+console.log('#page-banner has child nodes:', banner.hasChildNodes());
 
-console.log('all node children:');
-Array.from(bookList.childNodes).forEach(function(node){
-  console.log(node);
-});
-
-console.log('all element children:');
-Array.from(bookList.children).forEach(function(node){
-  console.log(node);
-});
-
-const titles = bookList.querySelectorAll('.name');
-
-console.log('Book titles:');
-Array.from(titles).forEach(function(title){
-  console.log(title.textContent);
-});
+const clonedBanner = banner.cloneNode(true);
+console.log(clonedBanner);
